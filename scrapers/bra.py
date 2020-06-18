fda1cd9a73d3ba607d6ebc1feba86911585ee30b
Dilcia Mercedes <dilcia19@stanford.edu>
2020-06-17 17:47:46 -0700

    small tweak

--- scrapers/bra.py
+++ scrapers/bra.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 
 def brazil():
 
+    
+
     # browser = webdriver.Chrome('/Users/dilcia_mercedes/Big_Local_News/prog/pitch_intl/PITCH/chromedriver') 
     # browser.get('https://covid.saude.gov.br/')
     # elem = browser.find_element(By.XPATH, "/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/div[1]/div[2]/ion-button//button")
