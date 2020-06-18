4c54a22b87eb5c4a48bf52bfb99f6ba9eb65ff2a
Serdar Tumgoren <zstumgoren@gmail.com>
2020-06-18 08:13:04 -0700

    Working version of Brazil scraper

--- scrapers/bra.py
+++ scrapers/bra.py
@@ -1,24 +1,48 @@
+import os
+import time
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
-def brazil():
 
-    
+def scrape():
+    url = 'https://covid.saude.gov.br/'
+    opts = Options()
+    opts.headless = True
+    data = []
+    try:
+        driver = webdriver.Firefox(
+            firefox_profile=ff_profile(),
+            options=opts
+        )
+        driver.get(url)
+        buttons = driver.find_elements_by_tag_name('ion-button')
+        for button in buttons:
+            if button.text.lower().strip() == 'arquivo csv':
+                button.click()
+                # NOTE: Timer needed to give download
+                # time to complete
+                time.sleep(10)
+                break
+    finally:
+        driver.quit()
 
-    # browser = webdriver.Chrome('/Users/dilcia_mercedes/Big_Local_News/prog/pitch_intl/PITCH/chromedriver') 
-    # browser.get('https://covid.saude.gov.br/')
-    # elem = browser.find_element(By.XPATH, "/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/div[1]/div[2]/ion-button//button")
-    # print(elem)
 
-    # other = browser.find_elements(By.XPATH, '//button')
-    # print(other)
-
-    #says no such element exists because of the shadow root issue
+def ff_profile(download_dir=os.getcwd()):
+    # Configure Firefox profile to avoid triggering pop-up
+    # that requests permission to download, per Selenium docs:
+    # https://selenium-python.readthedocs.io/faq.html#how-to-auto-save-files-using-custom-firefox-profile
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("browser.download.folderList", 2)
+    fp.set_preference("browser.download.manager.showWhenStarting", False)
+    fp.set_preference("browser.download.dir", download_dir)
+    fp.set_preference(
+        "browser.helperApps.neverAsk.saveToDisk",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+    return fp
 
 
 
 if __name__ == '__main__':
-    brazil()
-
-
-
+    scrape()
