033f5cbba7c265791ad21d27b81eb383309b8ca9
Dilcia Mercedes <dilcia19@stanford.edu>
2020-06-23 19:38:07 -0700

    porting South Africa scraper

--- covid_world_scraper/zaf.py
+++ covid_world_scraper/zaf.py
@@ -1,86 +1,98 @@
-from bs4 import BeautifulSoup
-# from old_germany import move_data_ger
-import requests
+"""
+Official page for South Africa COVID figures:
+
+    https://sacoronavirus.co.za/category/press-releases-and-notices/
+
+"""
+import csv
+import logging
 import os
-import pandas as pd
+import requests
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+
+from .country_scraper import CountryScraper
+
+logger = logging.getLogger(__name__)
+
+class Zaf(CountryScraper):
+
+    def fetch(self):
+
+        orig_url = 'https://sacoronavirus.co.za/category/press-releases-and-notices/'
+        orig_page = requests.get(orig_url)
+
+        soup = BeautifulSoup(orig_page.text, 'html.parser')
+        div = soup.find("div", class_="fusion-rollover")
+        link_tag = div.find("a")
+        link = link_tag.get("href")
+
+        page = requests.get(link)
+        saved_file = self.save_to_raw_cache(page.text, 'html')
+        return saved_file
+
+    def extract(self, source_file):
+
+        covid_deaths_list = []
+        covid_cases_list = []
+
+        with open(source_file) as html_file:
+            soup = BeautifulSoup(html_file.read(), 'html.parser')
+
+            # Extracting cases data 
+
+            tables = soup.find_all('table')
+            covid_cases = tables[0]
+
+            covid_table_row = covid_cases.find_all('tr')
+            for tr in covid_table_row:
+
+                text_list = []
+                td = tr.find_all('td')
+                for data in td:
+                    text_list.append(data.text)
+                covid_cases_list.append(text_list)
+
+            # Extracting deaths data
+
+            tables = soup.find_all('table')
+            covid_deaths = tables[1]
+
+            deaths_table_row = covid_deaths.find_all('tr')
+            for tr in deaths_table_row:
+                text_list = []
+                td = tr.find_all('td')
+                for data in td:
+                    text_list.append(data.text)
+                covid_deaths_list.append(text_list)
+                
+
+        covid_cases_header = covid_cases_list[0]
+        covid_cases_list = covid_cases_list[1:]
+        covid_deaths_header = covid_deaths_list[0]
+        covid_deaths_list = covid_deaths_list[1:]
+
+        basename = source_file.split('/')[-1].replace('.html','_cases.csv')
+        cases_outfile = str(self.processed_dir.joinpath(basename))
+        self._write_csv(covid_cases_header, covid_cases_list, cases_outfile)
+        logger.info('Created {}'.format(cases_outfile))
+
+        deaths_basename = source_file.split('/')[-1].replace('.html','_deaths.csv')
+        deaths_outfile = str(self.processed_dir.joinpath(deaths_basename))
+        self._write_csv(covid_deaths_header, covid_deaths_list, deaths_outfile)
+        logger.info('Created {}'.format(deaths_outfile))
+
+        return cases_outfile, deaths_outfile
 
 
-def south_africa():
+    def _write_csv(self, header, data, outfile):
 
-    orig_url = 'https://sacoronavirus.co.za/category/press-releases-and-notices/'
-    orig_page = requests.get(orig_url)
-
-    soup = BeautifulSoup(orig_page.text, 'html.parser')
-    div = soup.find("div", class_="fusion-rollover")
-    link_tag = div.find("a")
-    link = link_tag.get("href")
-    print(link)
-    
-    page = requests.get(link)
-    new_soup = BeautifulSoup(page.text, 'html.parser')
-    tables = new_soup.find_all('table')
-    covid_cases = tables[0]
-    covid_deaths = tables[1]
-    
-    covid_cases_list = []
-
-    covid_table_row = covid_cases.find_all('tr')
-    for tr in covid_table_row:
-
-        text_list = []
-        td = tr.find_all('td')
-        for data in td:
-            text_list.append(data.text)
-        covid_cases_list.append(text_list)
-
-    covid_cases_header = covid_cases_list[0]
-    covid_cases_list = covid_cases_list[1:]
-
-
-    covid_deaths_list = []
-    deaths_table_row = covid_deaths.find_all('tr')
-    for tr in deaths_table_row:
-        text_list = []
-        td = tr.find_all('td')
-        for data in td:
-            text_list.append(data.text)
-        covid_deaths_list.append(text_list)
-
-    covid_deaths_header = covid_deaths_list[0]
-    covid_deaths_list = covid_deaths_list[1:]
-
-    SA_cases_df = pd.DataFrame(covid_cases_list, columns = covid_cases_header)
-    SA_deaths_df = pd.DataFrame(covid_deaths_list, columns = covid_deaths_header)
-
-    SA_cases_df['Percentage total'] = SA_cases_df['Percentage total'].str.replace(',', '.')
-
-    remove_base = link.split('https://sacoronavirus.co.za/')
-    half_link = remove_base[1].split('/update')
-    date = half_link[0]
-    parsed_date = date.split('/')
-    year = parsed_date[0]
-    month = parsed_date[1]
-    day = parsed_date[2]
-    print(parsed_date)
-    print(link)
-
-    cases_file_name = f'{year}-{month}-{day}_south_africa_covid19_cases.csv'
-    deaths_file_name = f'{year}-{month}-{day}_south_africa_covid19_deaths.csv'
-    base_path = os.environ['TO_DATA_DIR']
-    cases_full_path = f'{base_path}/{cases_file_name}'
-    deaths_full_path = f'{base_path}/{deaths_file_name}'
-    print(cases_full_path)
-    print(deaths_full_path)
-
-    SA_cases_df.to_csv(cases_full_path, index=False)
-    SA_deaths_df.to_csv(deaths_full_path, index=False)
-
-    # SA_cases_df.to_csv(f'/Users/dilcia_mercedes/Big_Local_News/prog/pitch_intl/PITCH/Data/{year}-{month}-{day}_south_africa_covid19.csv', index=False)
-    # SA_deaths_df.to_csv(f'/Users/dilcia_mercedes/Big_Local_News/prog/pitch_intl/PITCH/Data/{year}-{month}-{day}_south_africa_covid19_deaths.csv', index=False)
-
-    # move_data_ger()
-
-
-if __name__ == '__main__':
-    south_africa()
-
+        with open(outfile,'w') as out:
+            writer = csv.writer(out)
+            writer.writerow(header)
+            writer.writerows(data)
+        logger.info("Save extracted data to {}".format(outfile))
