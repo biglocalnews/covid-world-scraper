075ad82b77e6a96bcba9f1fda44db60789a24a29
Dilcia Mercedes <dilcia19@stanford.edu>
2020-06-23 18:25:17 -0700

    porting nga scraper

--- covid_world_scraper/nga.py
+++ covid_world_scraper/nga.py
@@ -1,57 +1,89 @@
-from bs4 import BeautifulSoup
-# from old_germany import move_data_ger
-import requests
+"""
+Official page for Nigeria COVID figures:
+
+    https://covid19.ncdc.gov.ng/
+
+"""
+import csv
+import logging
 import os
-import pandas as pd
+import time
 
-def nigeria():
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
-    url = 'https://covid19.ncdc.gov.ng/'
-    data = requests.get(url)
+from .country_scraper import CountryScraper
 
-    soup = BeautifulSoup(data.text, 'html.parser')
+logger = logging.getLogger(__name__)
+
+class Nga(CountryScraper):
+
+    def fetch(self):
+
+        url = 'https://covid19.ncdc.gov.ng/'
+        opts = Options()
+        opts.headless = True
+        data = []
+        try:
+            driver = webdriver.Firefox(options=opts)
+            driver.get(url)
+            driver.get_screenshot_as_file(self._screenshot_path)
+            logger.info("Saved screenshot of web page to {}".format(self._screenshot_path))
+            table = driver.find_element_by_tag_name("table")
+            inner_html = table.get_attribute('innerHTML')
+            inner_text = table.get_attribute('innerText')
+            cached_html_path = self.save_to_raw_cache(inner_html, 'html')
+            cached_text_path = self.save_to_raw_cache(inner_text, 'txt')
+        finally:
+            driver.quit()
+        return cached_text_path
+
+
+    def extract(self, source_file):
+
+        nga_header = []
+        nga_data = []
+        with open(source_file, 'r') as infile:
+            for line in infile:
+                if 'States Affected' in line:
+                    line = line.replace('\n', '')
+                    line = line.split('\t')
+                    nga_header.append(line)
+                else:
+                    line = line.replace(',','')
+                    line = line.split()
+                    if len(line) > 5:
+                        line[0] = f'{line[0]} {line[1]}'
+                        line.pop(1)
+                    nga_data.append(line)
+
+            
+            for row in nga_data:
+                counter = 0
+                for item in row:
+                    if item.isnumeric():
+                        item = int(item)
+                        row[counter] = item
+                        counter += 1
+                    else:
+                        item = str(item)
+                        row[counter] = item
+                        counter += 1
+
+        basename = source_file.split('/')[-1].replace('txt','csv')
+        outfile = str(self.processed_dir.joinpath(basename))
+        self._write_csv(nga_header, nga_data, outfile)
+        logger.info('Created {}'.format(outfile))
+        return outfile
+
+
+    def _write_csv(self, header, data, outfile):
+
+        with open(outfile,'w') as out:
+            writer = csv.writer(out)
+            writer.writerow(header[0])
+            writer.writerows(data)
+        logger.info("Save extracted data to {}".format(outfile))
+
     
-    table = soup.table
-    
-    data_list = []
-    headers = []
-    table_row = table.find_all('tr')
-    
-    for tr in table_row:
-
-        th = tr.find_all('th')
-        for header in th:
-            headers.append(header.text)
-
-        text_list = []
-        td = tr.find_all('td')
-        for data in td:
-            text_list.append(data.text)
-        data_list.append(text_list)
-
-    data_list = data_list[1:]
-    
-    for data_row in data_list:
-        counter = 0
-        for data in data_row:
-            data = data.replace('\n', '')
-            data_row[counter] = data
-            counter += 1
-
-
-    
-    nigeria_covid_df = pd.DataFrame(data_list, columns = headers)
-    file_name = 'nigeria_covid19.csv'
-    base_path = os.environ['TO_DATA_DIR']
-    full_path = f'{base_path}/{file_name}'
-
-    nigeria_covid_df.to_csv(full_path, index=False)
-
-    # nigeria_covid_df.to_csv('/Users/dilcia_mercedes/Big_Local_News/prog/pitch_intl/PITCH/Data/nigeria_covid19.csv', index=False)
-
-    # move_data_ger()
-    
-
-
-if __name__ == '__main__':
-    nigeria()
