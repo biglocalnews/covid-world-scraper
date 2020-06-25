2649f7203b396579e56bfaa8c06f70e31c36eb00
Dilcia Mercedes <dilcia19@stanford.edu>
2020-06-25 15:31:09 -0700

    adding timestamp for india

--- covid_world_scraper/ind.py
+++ covid_world_scraper/ind.py
@@ -32,17 +32,30 @@ class Ind(CountryScraper):
         with open(raw_data_path) as fh:
             soup = BeautifulSoup(fh.read(), 'html.parser')
             headers = [h.text for h in soup.table.thead.find_all('th')]
+            headers.extend(["Date", "Scrape_Date"])
             data = []
+            date = soup.find("div", class_="status-update")
+            
+            date = date.text.split(':', 1)[1]
+            date = date.replace('\n', '')
+            date = date.replace('\t', '')
+
+            scrape_date = self.runtimestamp
+
             tbody_rows = soup.table.tbody.find_all('tr')
             for tr in tbody_rows:
                 cells = [cell.text.strip() for cell in tr.find_all('td')]
                 if self._is_data_row(cells):
+                    cells.extend([date])
+                    cells.extend([scrape_date])
                     data.append(cells)
                 else:
                     # footnote cells at bottom of table
                     # signal end of relevant data, so
                     # break to avoid extra loops
                     break
+
+                
         outfile = self.processed_filepath_from_raw(raw_data_path, 'csv')
         merged_data = [headers]
         merged_data.extend(data)
