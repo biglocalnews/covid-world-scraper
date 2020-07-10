ade0f826e81da56b6419dffff3842084b1b9c59c
Serdar Tumgoren <zstumgoren@gmail.com>
2020-07-10 14:22:06 -0700

    Add test for cache dir creation

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
+from pathlib import Path
 from click.testing import CliRunner
 
-
+from .conftest import cache_root
 from covid_world_scraper.cli import cli
 
 def test_list_countries():
@@ -10,6 +11,14 @@ def test_list_countries():
     assert '- BRA (Brazil)' in result.output
     assert '- PAK (Pakistan)' in result.output
 
-#TODO
-def test_cache_dir_creation():
-    pass
+def test_cache_dir_creation(cache_root):
+    cache_dir = Path(cache_root).joinpath('covid-world-scraper-data')
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            '--cache-dir={}'.format(str(cache_dir)),
+            '--list-scrapers'
+        ]
+    )
+    assert cache_dir.exists
